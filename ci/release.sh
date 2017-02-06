e475ad7c35f44433d7e3e745b6168d8b30de9b47
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@schibsted.com>
2017-02-06 16:52:29 +0100

    Setup CI.

---
+++ ci/release.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -e
+
+BRANCH_NAME=${TRAVIS_BRANCH}
+ARTIFACT_VERSION=undefined
+
+uploadReleaseToGitHub() {
+    git fetch --tags
+    LAST_TAG=$(git describe --tags --abbrev=0)
+    THIS_RELEASE=$(git rev-parse --short ${BRANCH_NAME})
+    local IFS=$'\n'
+    RELEASE_NOTES_ARRAY=($(git log --format=%B $LAST_TAG..$THIS_RELEASE | grep "*" | tr -d '\r'))
+    for i in "${RELEASE_NOTES_ARRAY[@]}"
+    do
+        RELEASE_NOTES="$RELEASE_NOTES\\n$i"
+    done
+
+    BODY="{
+        \"tag_name\": \"$ARTIFACT_VERSION\",
+        \"target_commitish\": \"$BRANCH_NAME\",
+        \"name\": \"$ARTIFACT_VERSION\",
+        \"body\": \"$RELEASE_NOTES\"
+    }"
+
+    # Create the release in GitHub and extract its id from the response
+    RESPONSE_BODY=$(curl \
+            -u ${REPO_USER}:${GITHUB_TOKEN} \
+            --header "Accept: application/vnd.github.v3+json" \
+            --header "Content-Type: application/json; charset=utf-8" \
+            --request POST \
+            --data "${BODY}" \
+            https://api.github.com/repos/"${TRAVIS_REPO_SLUG}"/releases)
+
+    # Extract the upload_url value
+    UPLOAD_URL=$(echo ${RESPONSE_BODY} | python -c 'import sys, json; print json.load(sys.stdin)[sys.argv[1]]' upload_url)
+    # And replace the end of it, which is generic and useless, by a relevant one
+    UPLOAD_URL=$(echo ${UPLOAD_URL} | sed 's/{?name,label}/?name=app-debug.aar/')
+
+    # Build the aar
+    ./gradlew :sdk:assembleImplementedDebug
+    # Copy it out of its cave
+    cp sdk/build/outputs/aar/app-implemented-debug.apk app-debug.apk
+
+    # Attach the artifact
+    curl -D - \
+    -u ${REPO_USER}:${GITHUB_TOKEN} \
+    --header "Accept: application/vnd.github.v3+json" \
+    --header "Content-Type: application/zip" \
+    --data-binary "@app-debug.apk" \
+    --request POST \
+    ${UPLOAD_URL}
+}
+
+case ${BRANCH_NAME} in
+    "master")
+        ./gradlew :sdk:assembleImplementedDebug
+        ARTIFACT_VERSION=$(git rev-list --count HEAD)
+        uploadReleaseToGitHub
+        ;;
+    *)
+        echo "Branch is ${BRANCH_NAME}, which is not releasable. Skipping release."
+        exit 0
+        ;;
+esac

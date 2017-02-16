8fc470f63711c33cdf09510f2ab62009cefa21fe
Jorge Antonio Díaz-Benito Soriano <stoyicker@users.noreply.github.com>
2017-02-16 18:39:49 +0100

    Tentative version of the data module. (#7)

--- app/src/main/kotlin/app/MainActivity.kt
+++ app/src/main/kotlin/app/MainActivity.kt
@@ -2,12 +2,12 @@ package app
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
-import org.jorge.ms.domain.HelloWorldKotlin
 
+/**
+ * Main Activity.
+ */
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        HelloWorldKotlin().let { Log.d("BANANAS", it.toString()) }
     }
 }
--- domain/src/main/kotlin/org/jorge/ms/domain/HelloWorldKotlin.kt
+++
@@ -1,9 +0,0 @@
-package org.jorge.ms.domain
-
-class HelloWorldKotlin {
-    companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            println("Hello, Kotlin")
-        }
-    }
-}

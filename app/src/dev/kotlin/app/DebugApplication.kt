1e94683456eef32d273569744773b6606d2f36e3
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-03-10 15:19:21 +0100

    VM StrictMode.

---
+++ app/src/dev/kotlin/app/DebugApplication.kt
@@ -0,0 +1,40 @@
+package app
+
+import android.os.StrictMode
+
+/**
+ * Debug application, for (guess what?!) debugging purposes.
+ */
+internal class DebugApplication : MainApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        enforceThreadStrictMode()
+        enforceVMStrictMode()
+    }
+
+    /**
+     * Enforces StrictMode for the current thread.
+     * @see <a href="https://developer.android.com/reference/android/os/StrictMode.html">
+     *     Strict Mode | Android Developers</a>
+     */
+    private fun enforceThreadStrictMode() {
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .penaltyDialog()
+                .build())
+    }
+
+    /**
+     * Enforces StrictMode for the VM.
+     * @see <a href="https://developer.android.com/reference/android/os/StrictMode.html">
+     *     Strict Mode | Android Developers</a>
+     */
+    private fun enforceVMStrictMode() {
+        StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .penaltyDeath()
+                .build())
+    }
+}

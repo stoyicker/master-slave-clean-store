9d473c9b68832aba699048e23e4825f48f43cca8
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-03-16 10:58:05 +0100

    Move android utils to util-android module.

--- app/src/main/kotlin/app/gaming/TopGamingAllTimePostsContentViewConfig.kt
+++ app/src/main/kotlin/app/gaming/TopGamingAllTimePostsContentViewConfig.kt
@@ -9,8 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.TextView
-import app.ext.getDimension
-import app.ext.isPortrait
+import util.android.ext.getDimension
+import util.android.ext.isPortrait
 import domain.entity.Post
 import org.jorge.ms.app.R
 
--- util-android/src/main/kotlin/util/android/ext/Context.kt
+++ util-android/src/main/kotlin/util/android/ext/Context.kt
@@ -1,4 +1,4 @@
-package app.ext
+package util.android.ext
 
 import android.content.Context
 import android.os.Build
@@ -10,13 +10,13 @@ import android.support.annotation.IntegerRes
  * Extension function to reduce verbosity to getInteger(...).
  * @param resId The id of the resource to get.
  */
-internal fun Context.getInteger(@IntegerRes resId: Int) = this.resources.getInteger(resId)
+fun Context.getInteger(@IntegerRes resId: Int) = this.resources.getInteger(resId)
 
 /**
  * Extension function to reduce verbosity to getColor(...).
  * @param resId The id of the resource to get.
  */
-internal fun Context.getColorCompat(@ColorRes resId: Int) =
+fun Context.getColorCompat(@ColorRes resId: Int) =
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M)
             this.getColor(resId)
         else
@@ -27,11 +27,11 @@ internal fun Context.getColorCompat(@ColorRes resId: Int) =
  * Extension function to reduce verbosity to getDimension(...).
  * @param resId The id of the resource to get.
  */
-internal fun Context.getDimension(@DimenRes resId: Int) = this.resources.getDimension(resId)
+fun Context.getDimension(@DimenRes resId: Int) = this.resources.getDimension(resId)
 
 /**
  * Extension function to check if the device is in portrait mode.
  */
-internal fun Context.isPortrait() = this.resources.displayMetrics.let {
+fun Context.isPortrait() = this.resources.displayMetrics.let {
     it.heightPixels > it.widthPixels
 }
